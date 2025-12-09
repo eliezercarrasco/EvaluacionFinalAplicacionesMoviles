4cfa867d375cb8eff113b0f443be8ea99f1f766e
Maria0412 <kothagatica@gmail.com>
2025-12-09 15:38:47 -0300

    Update FormularioValidacionTest.kt
    El test verifica que, al cargar la pantalla FormularioValidacion, la barra superior (TopBar) muestre correctamente el texto "Registro de Usuario".
    Para hacerlo, inicializa la actividad de Compose, crea un NavController, renderiza el composable y luego busca un nodo con ese texto, comprobando que existe en la interfaz.

---
+++ PasteleriaKotlin/PasteleriaKotlin/app/src/androidTest/java/com/example/pasteleriayy/FormularioValidacionTest.kt
@@ -0,0 +1,29 @@
+package com.example.pasteleriayy
+
+import android.app.Activity
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
+import androidx.navigation.NavController
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.pasteleriayy.ui.screens.FormularioValidacion
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class FormularioValidacionInstrumentedTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>(ComponentActivity::class.java)
+    @Test
+    fun topBarShowsTitle() {
+        val activity = composeTestRule.activity
+        val nav = NavController(activity)
+        composeTestRule.setContent {
+            FormularioValidacion(navController = nav)
+        }
+        composeTestRule.onNodeWithText("Registro de Usuario").assertExists()
+    }
